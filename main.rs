3b01d906bded526cb25f4ed4dda4731b5c9d2c96
Eric Dattore <eric.dattore@gmail.com>
2019-10-27 10:56:20 -0600

    Fix tests
    Left code that would fail in the `main.rs` file.

--- main.rs
+++ main.rs
@@ -6,27 +6,4 @@
 // To run the code:
 //     $ cargo run
 
-use sorted::sorted;
-
-use std::fmt::{self, Display};
-use std::io;
-
-#[sorted]
-pub enum Error {
-    Fmt(fmt::Error),
-    Io(io::Error),
-}
-
-impl Display for Error {
-    #[sorted::check]
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        #[sorted]
-            match self {
-            _ => write!(f, "unsupported"),
-            Error::Fmt(e) => write!(f, "{}", e),
-            Error::Io(e) => write!(f, "{}", e),
-        }
-    }
-}
-
 fn main() {}
